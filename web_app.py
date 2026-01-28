29de0bbabb9bcf2fce9ae384392cce6982717329
srobian032-blip <srobian032@gmail.com>
2026-01-28 07:04:57 +0000

    fix

--- web_app.py
+++ web_app.py
@@ -2,31 +2,22 @@ import os
 from flask import Flask, render_template, request, jsonify
 from core import get_bot_reply
 
-
 app = Flask(__name__)
-from flask import send_from_directory
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(
-        os.path.join(app.root_path, 'static'),
-        'favicon.ico',
-        mimetype='image/vnd.microsoft.icon'
-    )
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 @app.route("/chat", methods=["POST"])
 def chat():
-    data = request.get_json() or {}
-    user_message = data.get("message", "")
-    reply = get_bot_reply(user_message)
-    return jsonify({"reply": reply})
-
+    try:
+        data = request.get_json() or {}
+        user_message = data.get("message", "")
+        reply = get_bot_reply(user_message)
+        return jsonify({"reply": reply})
+    except Exception as e:
+        return jsonify({"reply": str(e)}), 500
 
 if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
